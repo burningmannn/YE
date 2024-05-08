68a39dcdc4674d743c774ce422c545c368cb1d79
burningmannn <daniil24022002@gmail.com>
2024-05-09 04:57:13 +0500

    Add Icon and Fonts

---
+++ YE/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  YE
+//
+//  Created by Daniil Shutkin on 08.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let label = UILabel()
+        label.text = "Danya"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20)
+        ])
+        
+        label.font = UIFont(name: "Coda", size: 40)
+        label.textColor = .white
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
