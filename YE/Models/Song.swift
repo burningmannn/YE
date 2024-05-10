d021df62e6be49d9e1704bc628ab3f212c1a4f78
burningmannn <daniil24022002@gmail.com>
2024-05-10 23:47:06 +0500

    Update Fonts and Create Album Interface

---
+++ YE/Models/Song.swift
@@ -0,0 +1,16 @@
+//
+//  Song.swift
+//  YE
+//
+//  Created by Daniil Shutkin on 10.05.2024.
+//
+
+import Foundation
+
+struct Song {
+    var name: String
+    var imageSong: String
+    var artist: String
+    var favorite: Bool
+    var fileName: String
+}
